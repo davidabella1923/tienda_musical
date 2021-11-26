a53293d9e1e6c37ffe44bf6f12012e3778a40112
davidabella1923 <davidavellamora@gmail.com>
2021-11-25 22:08:33 -0500

    bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/home/home.component';
+import { ContactComponent } from './Components/contact/contact.component';
+import { LoginComponent } from './Components/login/login.component';
+import { ShoppingComponent } from './Components/shopping/shopping.component';
+import { CatalogueComponent } from './Components/catalogue/catalogue.component';
+import { FooterComponent } from './Shared/footer/footer.component';
+import { NavbarComponent } from './Shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    LoginComponent,
+    ShoppingComponent,
+    CatalogueComponent,
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,

e3dcf9a0eb5ea874099cc663c7db8d7b97487ca2
davidabella1923 <davidavellamora@gmail.com>
2021-12-10 09:55:00 -0500

    ajustes catalogo y accesorios

--- src/app/Components/accesories/accesories.component.ts
+++ src/app/Components/accesories/accesories.component.ts
@@ -14,64 +14,196 @@ export class AccesoriesComponent implements OnInit {
 
   instrumentos:any = [
     {
-      "title":"Afinador Electronico",
+      "title":"AFINADOR ELECTRICO",
       "Description":"Cromático PW",
       "Imagen":"assets/img/catalogue/afinador1.jpg",
       "price":48000
     },
     {
-      "title":"Afinador Andario",
-      "Description":"Cromatico-Black",
+      "title":"AFINADOR ELECTRICO",
+      "Description":"Andario Cromatico-Black",
       "Imagen":"assets/img/catalogue/afinador2.jpg",
       "price":48500
     },
     {
-      "title":"Afinador Taylor",
-      "Description":"Texas ADK-E10W",
+      "title":"AFINADOR ELECTRICO",
+      "Description":"Texas Tylor ADK-E10W",
       "Imagen":"assets/img/catalogue/afinador3.jpg",
       "price":368000
     },{
-      "title":"Afinor Electronico ",
+      "title":"AFINADOR ELECTRICO ",
       "Description":"Cromatico, Black",
       "Imagen":"assets/img/catalogue/afinador4.jpg",
       "price":74200
     },
     {
-      "title":"Guitarra Eléctrica ",
-      "Description":"Washburn S1TS",
-      "Imagen":"assets/img/catalogue/guitarra5.jpg",
-      "price":602000
+      "title":"SILLA PARA PIANO ",
+      "Description":"Negra /Cafe",
+      "Imagen":"assets/img/catalogue/silla piano.png",
+      "price": 170000
     },
     {
-      "title":"Guitarra Eléctrica ",
-      "Description":"Washburn S1TS-Black",
-      "Imagen":"assets/img/catalogue/guitarra6.jpg",
-      "price":605000
+      "title":"EQUALIZADOR DE GUITARRA",
+      "Description":" 4 bandas Joyo JE-305",
+      "Imagen":"assets/img/catalogue/equalizador guitarra.jpg",
+      "price":70000
     },
     {
-      "title":"Bajo Eléctrico ",
-      "Description":"Warwick, Blak",
-      "Imagen":"assets/img/catalogue/bajo1.jpg",
-      "price":3798000
+      "title":"CAPO PARA GUITARRA ",
+      "Description":"Texas Plateado GCP8s",
+      "Imagen":"assets/img/catalogue/capo guitarra.png",
+      "price":45000
     },
     {
-      "title":"Bajo Eléctrico ",
-      "Description":"Warwick RB Corvette",
-      "Imagen":"assets/img/catalogue/bajo2.jpg",
-      "price":3456000
+      "title":"CAPO PARA GUITARRA ",
+      "Description":"Tylor 6 cuerdas Black Nickel",
+      "Imagen":"assets/img/catalogue/capo tylor.jpg",
+      "price":135000
     },
     {
-      "title":"Bajo electrico Texas",
-      "Description":"ADKE88BP-MWR",
-      "Imagen":"assets/img/catalogue/bajo3.jpg",
-      "price":731000
+      "title":"AUDIFONOS  SENNHEISER",
+      "Description":"Audifonos intraaurales y Dinamicos",
+      "Imagen":"assets/img/catalogue/audifonos sennheiser.png",
+      "price": 450000
     },
     {
-      "title":"Bajo Eléctrico  ",
-      "Description":"Gibson-Natural",
-      "Imagen":"assets/img/catalogue/bajo4.jpg",
-      "price": 5757000
-    }
+      "title":"AUDIFONOS SENNHEISER  ",
+      "Description":"Audifonos HD 25 Plus On Ear",
+      "Imagen":"assets/img/catalogue/audifono diadema.jpg",
+      "price": 1250000
+    },
+    {
+      "title":"STAND PARA MICROFONO",
+      "Description":"Takstar 360 Grados",
+      "Imagen":"assets/img/catalogue/stand para microfono.png",
+      "price":135000
+    },
+    {
+      "title":"INTERFAZ PARA IPHONE ",
+      "Description":"Interfaz Iphone/Ipad/Ipod Sonic Port",
+      "Imagen":"assets/img/catalogue/interfaz iphone.png",
+      "price":480000
+    },
+    {
+      "title":"PICKS GUITARRA ",
+      "Description":"picks Blackline Alan Forbes 6 Unidades",
+      "Imagen":"assets/img/catalogue/picks guitarra.jpg",
+      "price":19500
+    },
+    {
+      "title":"PICKS DELUXE ",
+      "Description":"Tylor Picks Koa Collector",
+      "Imagen":"assets/img/catalogue/picks deluxe.png",
+      "price":185000
+    },
+    {
+      "title":"GRIP EJERCITADOR ",
+      "Description":"Planet Waves PW-VG-01",
+      "Imagen":"assets/img/catalogue/grip ejercitador.jpg",
+      "price":56000
+    },
+    {
+      "title":"TAPONES OIDOS ",
+      "Description":"tapones para oidos Zildjian",
+      "Imagen":"assets/img/catalogue/tapones oidos.png",
+      "price":85000
+    },
+    {
+      "title":"PEDAL FOOTSWITCH PARA GUITARRA ",
+      "Description":"Express MKII Line 6 ",
+      "Imagen":"assets/img/catalogue/pedal footswitch.jpg",
+      "price":685000
+    },
+    {
+      "title":"BOQUILLA SAXO TENOR ",
+      "Description":"Graftonite B3 Rico ",
+      "Imagen":"assets/img/catalogue/boquilla saxo.png",
+      "price":153000
+    },
+    {
+      "title":"ABRAZADERA RICO ROYAL",
+      "Description":"tapaboquilla Para Saxo Tenor",
+      "Imagen":"assets/img/catalogue/abrazadera royal.jpg",
+      "price":98500
+    },
+    {
+      "title":"MICROFONO INALAMBRICO",
+      "Description":"Guitarra/Bajo line 6 Relay G30",
+      "Imagen":"assets/img/catalogue/microfono inalambrico.png",
+      "price":998000
+    },
+    {
+      "title":"CABLE ROXTONE ",
+      "Description":"3 mt Microfono  macho smxx200l3",
+      "Imagen":"assets/img/catalogue/cable roxtone.jpg",
+      "price":18650
+    },
+    {
+      "title":"CABLE ROXTONE ",
+      "Description":"Interpedales 1/4-1/4",
+      "Imagen":"assets/img/catalogue/cable 0.15.png",
+      "price":18450
+    },
+    {
+      "title":"BAQUETAS ",
+      "Description":"Punta de Madera Vic Firth N7A Nova",
+      "Imagen":"assets/img/catalogue/baquetas nova.png",
+      "price":22500
+    },
+    {
+      "title":"ESTUCHE PARA UKULELE ",
+      "Description":"Ukulele tenor Mahalo UB1",
+      "Imagen":"assets/img/catalogue/funda ukulele.png",
+      "price":98600
+    },
+    {
+      "title":"ESTUCHE RIGIDO ",
+      "Description":"personalizado, Hecho a Medida",
+      "Imagen":"assets/img/catalogue/estuche rigido.png",
+      "price":450000
+    },
+    {
+      "title":"ESTUCHE SEMI DURO ",
+      "Description":"Personalizado, Hecho a Medida",
+      "Imagen":"assets/img/catalogue/estuche semi.jpg",
+      "price":255000
+    },
+    {
+      "title":"PEDAL PARA SINTETIZADOR",
+      "Description":"Permite Graduar el Efecto Deseado",
+      "Imagen":"assets/img/catalogue/pedal sintetizador.png",
+      "price":655000
+    },
+    {
+      "title":"PARRILLA HOHNER ",
+      "Description":"Repuesto Corona III",
+      "Imagen":"assets/img/catalogue/repuesto hohner.png",
+      "price":157000
+    },
+    {
+      "title":"FUELLE HOHNER ",
+      "Description":"Repuesto Rey Vallenato ",
+      "Imagen":"assets/img/catalogue/repuesto fuelle.jpg",
+      "price":235000
+    },
+    {
+      "title":"FUELLE HOHNER ",
+      "Description":"Repuesto Corona III",
+      "Imagen":"assets/img/catalogue/repuesto fuelle2.png",
+      "price":685000
+    },
+    {
+      "title":"KIT ACCESORIOS FENDER ",
+      "Description":"Perillas y Partes Genuinas Blanco",
+      "Imagen":"assets/img/catalogue/kit accesorios fender.jpg",
+      "price":85000
+    },
+    {
+      "title":"LLAVE AFINACION BATERIA ",
+      "Description":"Evans DATK Torque",
+      "Imagen":"assets/img/catalogue/llave bateria.png",
+      "price":108500
+    },
   ]
 }
 
--- src/app/Components/catalogue/catalogue.component.ts
+++ src/app/Components/catalogue/catalogue.component.ts
@@ -11,65 +11,200 @@ export class CatalogueComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  instrumentos:any = [
+  instrumentos: any = [
+
+   
     {
-      "title":"Guitarra Clasica",
-      "Description":"Electroacústica Texas",
-      "Imagen":"assets/img/catalogue/guitarra1.jpg",
-      "price":330000
+      "title": "GUITARRA ELECTRICA ",
+      "Description": "Washburn S1TS",
+      "Imagen": "assets/img/catalogue/guitarra5.jpg",
+      "price": 602000
     },
     {
-      "title":"Guitarra Acústica",
-      "Description":"Marcate Ecológica",
-      "Imagen":"assets/img/catalogue/guitarra2.jpg",
-      "price":280000
+      "title": "GUITARRA ELECTRICA ",
+      "Description": "Washburn S1TS-Black",
+      "Imagen": "assets/img/catalogue/guitarra6.jpg",
+      "price": 605000
     },
     {
-      "title":"Guitarra electrica ",
-      "Description":"Texas ADK-E10W",
-      "Imagen":"assets/img/catalogue/guitarra3.jpg",
-      "price":368000
-    },{
-      "title":"Guitarra Eléctrica ",
-      "Description":"Texas ADK-E100, Black",
-      "Imagen":"assets/img/catalogue/guitarra4.jpg",
-      "price":370000
+      "title": "BAJO ELECTRICO ",
+      "Description": "Warwick, Blak",
+      "Imagen": "assets/img/catalogue/bajo1.jpg",
+      "price": 3798000
     },
     {
-      "title":"Guitarra Eléctrica ",
-      "Description":"Washburn S1TS",
-      "Imagen":"assets/img/catalogue/guitarra5.jpg",
-      "price":602000
+      "title": "BAJO ELECTRICO ",
+      "Description": "Warwick RB Corvette",
+      "Imagen": "assets/img/catalogue/bajo2.jpg",
+      "price": 3456000
     },
     {
-      "title":"Guitarra Eléctrica ",
-      "Description":"Washburn S1TS-Black",
-      "Imagen":"assets/img/catalogue/guitarra6.jpg",
-      "price":605000
+      "title": "BATERIA STANDAR NIÑO ",
+      "Description": "Mapex Prodigy series",
+      "Imagen": "assets/img/catalogue/Bateria standar2.png",
+      "price": 2175000
     },
     {
-      "title":"Bajo Eléctrico ",
-      "Description":"Warwick, Blak",
-      "Imagen":"assets/img/catalogue/bajo1.jpg",
-      "price":3798000
+      "title": "BATERIA CHERRY MIST MAPLE ",
+      "Description": "Saturn V Exotic Cherry",
+      "Imagen": "assets/img/catalogue/Bateria_3.png",
+      "price": 15203000
     },
     {
-      "title":"Bajo Eléctrico ",
-      "Description":"Warwick RB Corvette",
-      "Imagen":"assets/img/catalogue/bajo2.jpg",
-      "price":3456000
+      "title": "BATERIA YAMAHA  ",
+      "Description": "Drumbass IV",
+      "Imagen": "assets/img/catalogue/bateria1.jpg",
+      "price": 8564000
     },
     {
-      "title":"Bajo electrico Texas",
-      "Description":"ADKE88BP-MWR",
-      "Imagen":"assets/img/catalogue/bajo3.jpg",
-      "price":731000
+      "title": "BATERIA YAMAHA  ",
+      "Description": "Apocalipse Red Limited Edition",
+      "Imagen": "assets/img/catalogue/bateria2.jpg",
+      "price": 12639000
     },
     {
-      "title":"Bajo Eléctrico  ",
-      "Description":"Gibson-Natural",
-      "Imagen":"assets/img/catalogue/bajo4.jpg",
-      "price": 5757000
-    }
+      "title": "ACORDEON HOHNER  ",
+      "Description": "Bravo  II 48 bajos rojo",
+      "Imagen": "assets/img/catalogue/acordeon01.png",
+      "price": 3562000
+    },
+    {
+      "title": "ACORDEON HOHNER  ",
+      "Description": "Clasico Corona II",
+      "Imagen": "assets/img/catalogue/acordeon2.png",
+      "price": 2800000
+    },
+    {
+      "title": "ACORDEON HONNER ",
+      "Description": "Rey Vallenato Rojo",
+      "Imagen": "assets/img/catalogue/REYVALLENATOHOHNERROJO.jpg",
+      "price": 2670000
+    },
+    {
+      "title": "ACORDEON HOHNER ",
+      "Description": "BRAVO III  72 BAJOS ROJO",
+      "Imagen": "assets/img/catalogue/acordeon hohner bravo rojo.png",
+      "price": 4800000
+    },
+    {
+      "title": "PIANO DIGITAL ",
+      "Description": "KURZWEIL KA130",
+      "Imagen": "assets/img/catalogue/Piano digital KA130.jpg",
+      "price": 3300000
+    },
+    {
+      "title": "SINTETIZADOR ROLAND ",
+      "Description": " XPS-10 Profesional",
+      "Imagen": "assets/img/catalogue/Sintetizador Roland.png",
+      "price": 2500000
+    },
+    {
+      "title": "PIANO VERTICAL  ",
+      "Description": "ESSEX BY STEINWAY",
+      "Imagen": "assets/img/catalogue/piano vertical essex.jpg",
+      "price": 25900000
+    },
+    {
+      "title": "PIANO DE COLA ",
+      "Description": "By Steinway EGP155",
+      "Imagen": "assets/img/catalogue/piano de cola essex.jpg",
+      "price": 63500000
+    },
+    {
+      "title": "MICROFONO VOCAL TECHNICA ",
+      "Description": "Vocal Audio de Condensador",
+      "Imagen": "assets/img/catalogue/microfono vocal technica.png ",
+      "price": 500000
+    },
+    {
+      "title": "MICROFONO SENNHEISER ",
+      "Description": "cardioide para drums",
+      "Imagen": "assets/img/catalogue/microfono sennheiser.jpg",
+      "price": 700000
+    },
+    {
+      "title": "MICROFONO SENNHEISER SILVER ",
+      "Description": "Super Cardioide para Instrumento",
+      "Imagen": "assets/img/catalogue/mic sennheiser silver.png",
+      "price": 652000
+    },
+    {
+      "title": "SET DE MICROFONOS SENNHEISER ",
+      "Description": "Drummiccase para bateria de 7 unidades",
+      "Imagen": "assets/img/catalogue/set sennheiser.png",
+      "price": 6500000
+    },
+    {
+      "title": "CABINA DE SONIDO ",
+      "Description": "Cabina Line 6 Stagesourse",
+      "Imagen": "assets/img/catalogue/cabina line.jpg",
+      "price": 3450000
+    },
+    {
+      "title": "CABINA DE SONIDO  ",
+      "Description": "Cerwin Vega 1500 Watts",
+      "Imagen": "assets/img/catalogue/cabina cerwin.png",
+      "price": 3800000
+    },
+    {
+      "title": "CABINA DE SONIDO ",
+      "Description": "stagesourse activa 1400 Watts",
+      "Imagen": "assets/img/catalogue/cabina l3t.png",
+      "price": 4570000
+    },
+    {
+      "title": "CABINA DE SONIDO  ",
+      "Description": "Mackie Activa 1600 Watts",
+      "Imagen": "assets/img/catalogue/cabina suspendida.jpg",
+      "price": 5680000
+    },
+    {
+      "title": "AMPLIFICADOR DE GUITARRA  ",
+      "Description": "Firehawk 1500 line 6",
+      "Imagen": "assets/img/catalogue/amplificador guitarra.png",
+      "price": 650000
+    },
+    {
+      "title": "AMPLIFICADOR DE GUITARRA ",
+      "Description": "JC-120 jazz Chorus",
+      "Imagen": "assets/img/catalogue/amplificador roland.jpg",
+      "price": 5670000
+    },
+    {
+      "title": "AMPLIFICADOR DE GUITARRA ",
+      "Description": "Marshall 1962 BluesBreaker",
+      "Imagen": "assets/img/catalogue/amplificador marshall.png",
+      "price": 7893500
+    },
+    {
+      "title": "AMPLIFICADOR DE GUITARRA  ",
+      "Description": "Marshall AST2C 30 Watts Astoria",
+      "Imagen": "assets/img/catalogue/amplificador AST2C.png",
+      "price": 9200000
+    },
+    {
+      "title": "EFECTOS VOZ ",
+      "Description": "procesador de efecto vocal Boss VE-5RD",
+      "Imagen": "assets/img/catalogue/pedal boss.png",
+      "price": 1450000
+    },
+    {
+      "title": "EFECTOS VOZ ",
+      "Description": " procesador de efectos vocales Boss VE-20",
+      "Imagen": "assets/img/catalogue/pedal red boss.jpg",
+      "price": 1699000
+    },
+    {
+      "title": "EFECTOS VOZ ",
+      "Description": "procesador de efectos vocales Boss VE-8",
+      "Imagen": "assets/img/catalogue/pedal beige.png",
+      "price": 1780000
+    },
+    {
+      "title": "EFECTOS VOZ  ",
+      "Description": "Procesador de efectos vocales Boss VE-500",
+      "Imagen": "assets/img/catalogue/pedal gris.jpg",
+      "price": 2250000
+    },
   ]
 }

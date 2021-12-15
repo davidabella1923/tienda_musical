9330bb1d72702e9f4ffb38f247fa3b50e378d16c
Diego Anacona <diefer123anacona@gmail.com>
2021-12-14 20:56:39 -0500

    Ajuste Home

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -11,5 +11,37 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
   }
+tipoArticulos: any = [
+"assets/img/catalogue/cars1.png",
+"assets/img/catalogue/cars2.jpg",
+"assets/img/catalogue/cars3.jpg",
+"assets/img/catalogue/cars4.jpg",
+"assets/img/catalogue/cars5.jpg",
+"assets/img/catalogue/cars6.jpg"
 
+]
+cardCirculos: any = [
+  {
+    title: "AMPLIFICADOR",
+    ImagenCirculo:"assets/img/catalogue/img1.jpg"
+  },
+  {
+    title: "BOCINAS",
+    ImagenCirculo:"assets/img/catalogue/img2.jpg"
+  },
+  {
+    title: "REGULADOR",
+    ImagenCirculo:"assets/img/catalogue/img3.jpg"
+  },
+  {
+    title: "POTENCIADOR",
+    ImagenCirculo:"assets/img/catalogue/img4.jpg"
+  },
+  {
+    title: "MEZCLADOR",
+    ImagenCirculo:"assets/img/catalogue/img5.jpg"
+  }
+
+]
 }
+
